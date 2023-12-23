72594160357cdd42e80e22fd57118f67a619104d
Khoirul Umam <khoirul.umam35@gmail.com>
2023-12-23 17:45:40 +0000

    Tes SAW

--- Hello.py
+++ Hello.py
@@ -1,50 +1,46 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import numpy as np
 
-LOGGER = get_logger(__name__)
-
+if 'data' not in st.session_state:
+    st.session_state.data = np.array([0])
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Implementasi SAW",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
+    st.write("# Implementasi Metode SAW")
+    st.write("Dikembangkan oleh Khoirul Umam, S.Pd., M.Kom.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Metode Simple Additive Weighting (SAW) merupakan salah satu metode dalam Sistem Pendukung Keputusan yang implementasinya sangat sederhana. Ide besarnya adalah mengalikan nilai kriteria dengan bobot kriteria yang telah ditentukan. Kemudian hasil perkalian tersebut ditotal untuk mendapatkan nilai akhir. Berdasarkan nilai akhir tersebut, sekumpulan alternatif dapat diranking untuk menentukan alternatif terbaik.
+        
+        Contoh kasus yang akan diimplementasikan pada aplikasi ini adalah sebagai berikut:
+
+        Sebuah perusahaan akan melakukan rekrutmen kerja terhadap **5 calon pekerja** untuk posisi operator mesin. Posisi yang dibutuhkan hanya **2 orang**. Kriteria seleksi yang digunakan adalah sebagai berikut:
+
+        - Pengalaman kerja (C1), semakin lama pengalaman kerjanya semakin baik
+        - Pendidikan (C2), semakin tinggi pendidika terakhirnya semakin baik
+        - Usia (C3), semakin dewasa usianya semakin baik
+        - Status Perkawinan (C4), yang masih single lebih baik dari pada yang sudah menikah
+        - Alamat (C5), yang dekat domisilinya dengan kantor semakin baik.
+
     """
     )
+
+    c1 = st.number_input("Nilai C1", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+
+    st.write("Nilai C1: ", c1)
+
+    st.write("Data = ", st.session_state.data)
+
+    st.button("Simpan", on_click=simpanData(c1), type='primary')
+
+
+def simpanData(dataBaru):
+    np.append(st.session_state.data, dataBaru)
 
 
 if __name__ == "__main__":
