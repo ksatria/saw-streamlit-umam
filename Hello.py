5d5afaf5d246a32fd3ebf336d1ca881efb0dc754
Khoirul Umam <khoirul.umam35@gmail.com>
2023-12-24 15:25:49 +0000

    Penyempurnaan

--- Hello.py
+++ Hello.py
@@ -1,14 +1,64 @@
 import streamlit as st
 import numpy as np
-
-if 'data' not in st.session_state:
-    st.session_state.data = np.array([0])
+import pandas as pd
 
 if 'clicked' not in st.session_state:
     st.session_state.clicked = False
 
+L = np.array(['benefit','benefit','benefit','cost','cost'])
+
+W = np.array([0.3, 0.2, 0.2, 0.15, 0.15])
+
 def click_button():
     st.session_state.clicked = True
+
+def sample_norm(values, label):
+    if not values.shape[0] == label.shape[0]:
+        st.write('Jumlah kriteria dan label tidak sama')
+        return
+
+    norm_value = []
+    norm_all = []
+
+    for i in range(values.shape[0]):
+        if label[i] == 'benefit':
+            for j in range(values[i].shape[0]):
+                norm_c = values[i][j] / np.max(values[i])
+                norm_value.append(norm_c)
+        elif label[i] == 'cost':
+            for j in range(values[i].shape[0]):
+                norm_c = np.min(values[i]) / values[i][j]
+                norm_value.append(norm_c)
+
+        norm_all.append(norm_value)
+        norm_value = []
+
+    return np.array(norm_all)
+
+def calculate_saw(values, weight):
+    if not values.shape[0] == weight.shape[0]:
+        print('Jumlah kriteria dan bobot tidak sama')
+        return
+
+    alt_crit_value = []
+    all_value = []
+    all_saw = []
+
+    values = np.transpose(values)
+
+    for i in range(values.shape[0]):
+        for j in range(values[i].shape[0]):
+            val = values[i][j] * weight[j]
+            alt_crit_value.append(val)
+
+        all_value.append(alt_crit_value)
+        alt_crit_value = []
+
+        saw = np.sum(all_value)
+        all_saw.append(saw)
+        all_value = []
+
+    return all_saw
 
 def run():
     st.set_page_config(
@@ -36,22 +86,34 @@ def run():
     """
     )
 
-    c1 = st.number_input("Nilai C1", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    st.divider()
 
-    st.write("Nilai C1: ", c1)
+    st.write("## Input Nilai Kriteria")
+
+    c1 = st.number_input("Nilai C1", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    c2 = st.number_input("Nilai C2", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    c3 = st.number_input("Nilai C3", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    c4 = st.number_input("Nilai C4", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
+    c5 = st.number_input("Nilai C5", min_value=0.0, max_value=1.0, value=0.0, step=0.1)
 
     if st.button("Simpan", type='primary', on_click=click_button):
-        simpanData(c1)
+        simpanData(c1,c2,c3,c4,c5)
     
     if st.session_state.clicked:
-        st.write("Data = ", st.session_state.data)
+        data = st.session_state.nilai_kriteria
+        df = pd.DataFrame(data, columns=('C1','C2','C3','C4','C5'))
+        st.dataframe(df)
 
 
-def simpanData(dataBaru):
+def simpanData(c1,c2,c3,c4,c5):
     st.write("Simpan data baru")
-    dataLama = st.session_state.data
-    tambahan = np.append(dataLama, dataBaru)
-    st.session_state.data = tambahan
+
+    if 'nilai_kriteria' not in st.session_state:
+        st.session_state.nilai_kriteria = np.array([[c1,c2,c3,c4,c5]])
+    else:
+        dataLama = st.session_state.nilai_kriteria
+        dataBaru = np.append(dataLama, [[c1,c2,c3,c4,c5]], axis=0)
+        st.session_state.nilai_kriteria = dataBaru
 
 
 if __name__ == "__main__":
